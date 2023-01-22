ce779042deac597b47d30b35a6c84cf15a488c28
abbazsr795 <abbazsr795@gmail.com>
2023-01-22 10:32:25 +0530

    added the signin and signup modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './Components/SignIn.js'
+import SignUp from './Components/SignUp.js'
+import Footer from './Components/Footer.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div>
+      <SignUp/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/SignIn.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../index.css'
+import 'tachyons'
+
+const SignIn = () => {
+    return(
+        <div className="horizontalcenter">
+            <div className="emailandpasswordbox">
+                <div className="verticalcenter">
+                    <h1>Email</h1>
+                    <input className="inputbox" ></input>
+                    <h1>Password</h1>
+                    <input className="inputbox" type={"password"}></input>
+                    <br></br>
+                    <br></br>
+                   <div className="horizontalcenter">
+                        <button className="button1 grow">Sign In</button>
+                   </div>
+                   <br></br>
+                   <br></br>
+                    <a href="https://www.google.com/">Don't have an account?</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
