bf2c55ebeaaa7fb8338b37ef26ce4e4dd64e86b2
amraaz <xylome45a@gmail.com>
2023-01-23 16:31:40 +0530

    added firebase

---
+++ src/FbStuff/fb.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiCMaqaD0R3rc0W2rx40-H03jCOkPgaSc",
+  authDomain: "vaccinic-b6f0d.firebaseapp.com",
+  projectId: "vaccinic-b6f0d",
+  storageBucket: "vaccinic-b6f0d.appspot.com",
+  messagingSenderId: "14331050256",
+  appId: "1:14331050256:web:f9c2290bc4de9dd7af8d95"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
