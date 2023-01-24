ace9969fe28380bebeda8d7a0d642766e3f6f744
amraaz <xylome45a@gmail.com>
2023-01-24 05:30:44 +0530

    minor improvs and added home ( Base route ) component

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
+const Home = ()=>{
+
+    // let navigate = useNavigate()
+
+
+
+    return <>
+        <h1>
+            Home Page
+        </h1>
+    </>
+}
+
+export default Home
