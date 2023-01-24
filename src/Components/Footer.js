d26f96de86c6da79bf71b4084c952bcbcb9e48b9
amraaz <xylome45a@gmail.com>
2023-01-24 12:29:33 +0530

    added nasa API

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
 
     return(
         <div className="footer">
-            { rend ? <></> : <><button>Back {rend} </button></> }
+            { rend ? null : <><button>Back {rend} </button></> }
             <h3><a className="notextdeco" href="https://www.google.com/">Log Out</a></h3> 
             <h2 className="title">VACCINIC</h2>
             <h3><a className="notextdeco" href="https://www.google.com/">Abbazs Rahman</a></h3> {/*//This button basically brings a new page that allows the user to edit their personal info*/}
