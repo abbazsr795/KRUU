895000a4a548cdf555ae29666fa56ba93f4a83ff
amraaz <xylome45a@gmail.com>
2023-01-25 09:47:20 +0530

    added stuff

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom"
-import { RenderComp } from "../States/Misc"
+// import { RenderComp } from "../States/Misc"
 
 const Home = ()=>{
 
     let navigate = useNavigate()
-    
+
 
 
     let toDashboard = () =>{
@@ -18,16 +18,10 @@ const Home = ()=>{
 
 
     return <>
-        <h1>
-            Home Page
-        </h1>
+        <h1>Home Page</h1>
         <div>
-            <button onClick={()=>{toDashboard()}} >
-                Dashboard
-            </button>
-            <button onClick={()=>{toUserInfo()}} >
-                User Info
-            </button>
+            <button onClick={()=>{toDashboard()}} >Dashboard</button>
+            <button onClick={()=>{toUserInfo()}} >User Info</button>
         </div>
     </>
 }
