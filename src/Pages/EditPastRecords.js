b1c091ecbb909ff0e48e410c0ead9acd7d950fde
amraaz <xylome45a@gmail.com>
2023-01-31 07:31:08 +0530

    added date tiem picker

--- src/Pages/EditPastRecords.js
+++ src/Pages/EditPastRecords.js
@@ -1,18 +1,21 @@
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
 
 import { useState } from "react";
+import DatePicker from "react-date-picker";
+
 
 let EditPastRecords = ()=>{
 
-    const [startDate, setStartDate] = useState(new Date());
+    const [startDate, onChangeStart] = useState(new Date());
+    const [endDate, onChangeEnd] = useState(new Date());
 
 
     return <div className="has-text-centered" >
         <input type="text" placeholder="Vaccine Name" />
         <textarea name="Details" placeholder="Vaccine Details" id="" cols="30" rows="10"/>
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        <DatePicker onChange={onChangeStart} value={startDate} />
+        <br />
+        <br />
+        <DatePicker onChange={onChangeEnd} value={endDate} />
     </div>
 }
 
