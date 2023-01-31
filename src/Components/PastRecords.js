9cce46fe6f745e50c2a2ab17c5a8504db7a19eab
abbazsr795 <abbazsr795@gmail.com>
2023-01-31 16:21:55 +0530

    corrected minor errors

--- src/Components/PastRecords.js
+++ src/Components/PastRecords.js
@@ -28,7 +28,7 @@ let PastRecords = ()=>{
     return <>
         <div className="horizontalcenter flexiblerow spacearound">
             <button onClick={()=>{get()}} ><div className="verticalcenter card1 grow "><div className="horizontalcenter"><img className="img2" src="https://img.icons8.com/cotton/512/record.png"/></div><h1 className="midtext">Get Records</h1></div></button>
-            { isempty ? null : <button onClick={()=>{navigate('/pastrecords/edit')}} ><div className="verticalcenter card1 grow"><div className="horizontalcenter"><img className='img2' src="https://img.icons8.com/ios/512/plus-2-math.png"/></div><h1 className="midtext">Add Record</h1></div></button> }
+            { isempty ? null : <button onClick={()=>{navigate('/vaccines/pastrecords/edit')}} ><div className="verticalcenter card1 grow"><div className="horizontalcenter"><img className='img2' src="https://img.icons8.com/ios/512/plus-2-math.png"/></div><h1 className="midtext">Add Record</h1></div></button> }
         </div>
     </>
 }
