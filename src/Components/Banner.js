86c3a5596732e36c9dafdc7474b2164e50be37e4
abbazsr795 <abbazsr795@gmail.com>
2023-02-01 15:49:23 +0530

    cant do

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -5,19 +5,18 @@ import SignUpPage from "../Pages/SignUpPage";
 const Banner = () => {
     return(
         <div className="">
-            <h1 className="massivetext heading1 horizontalcenter"> Welcome to Vaccinic</h1>
             <div className = "stack pa4">
                 <div className="stack">
-                    <div className="row">
-                        <h1 className="bold ultramassivetext">Vaccinic is an all in one vaccine management tool</h1>
+                    <div className="flexiblerow">
+                        <h1 className="bold ultramassivetext bannertext1">Vaccinic is an all in one vaccine management tool</h1>
                         <img src="https://img.icons8.com/plasticine/512/clinic.png"/>
                     </div>
-                    <div className="row">
+                    <div className="flexiblerow banner2 ">
                         <img src="https://img.icons8.com/plasticine/512/syringe.png"/>
-                        <h1 className="bold ultramassivetext">Currently Vaccinic has over 15 different vaccines</h1>
+                        <h1 className="bold ultramassivetext bannertext1">Currently Vaccinic has over 15 different vaccines</h1>
                     </div>
                     <h1 className="heading2 horizontalcenter">With Vaccinic you can</h1>
-                    <div className="flexiblerow spacearound">
+                    <div className="flexiblerow spacearound ">
                         <div className="card2 grad1 stack pa3">
                             <img src="https://img.icons8.com/plasticine/512/treatment-plan.png"/>
                             <h1>Keepp track of your past vaccines</h1>
