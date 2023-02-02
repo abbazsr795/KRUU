40a4140d965c56f5bcdd17b9f24c66fc1594e194
amraaz <xylome45a@gmail.com>
2023-02-02 15:26:54 +0530

    d

--- src/Pages/AddVaccines.forIndexPage.js
+++ src/Pages/AddVaccines.forIndexPage.js
@@ -1,14 +1,53 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { db } from "../FbStuff/fb"
+import Select from 'react-select'
+import { collection, getDocs, query, addDoc } from "firebase/firestore"
 
 let AddVaccinesforIndex = ()=>{
 
+    let values = []
+
+
+    let [vaccselected,setSelected] = useState('')
     let [vaccname,setVaccName] = useState('')
     let [vaccdesc,setVaccDesc] = useState('')
+    let [vaccurl,setVaccurl] = useState('')
+    let [vaccregion,setVaccregion] = useState(["Northern Asia","Central Asia","South America","Western Asia", "Southern Asia","Eastern Asia", "South Eastern Asia","Europe","Oceania","Africa","North America"])
+
+    let add = async ()=>{
+        const docref = await addDoc(collection(db,"Index"),{
+            name: vaccname,
+            desc: vaccdesc,
+            url:  vaccurl,
+            vaccregion: vaccselected
+        })
+
+        if (docref!==""){
+            alert('success') 
+        }
+    }
+
+
+    let SelectedVal = (selectval)=>{
+        setSelected(selectval)
+        // alert(selectval.value)
+    }
+
+    useEffect(()=>{
+        for (let i = 0; i < vaccregion.length; i++) {
+            values.push({
+                value: vaccregion[i],
+                label: vaccregion[i]
+            })
+          }
+    },[vaccregion, values])
 
     return <div>
         <input value={vaccname} onchange={(event)=>{setVaccName(event.target.value)}} />
         <textarea value={vaccdesc} onchange={(event)=>{setVaccDesc(event.target.value)}} />
-
+        <textarea value={vaccurl} onchange={(event)=>{setVaccurl(event.target.value)}} />
+        <Select options={values} onChange={SelectedVal()} />
+        <button onClick={()=>{add()}} >Add</button>
     </div>
 
 }
