6600901fa3fe8677e1410902dfb3a1051909b702
abbazsr795 <abbazsr795@gmail.com>
2023-02-02 15:46:44 +0530

    half life

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,8 +4,8 @@ import SignUpPage from "../Pages/SignUpPage";
 
 const Banner = () => {
     return(
-        <div className="">
-            <div className = "stack pa4">
+        <div>
+            {/* <div className = "stack pa4">
                 <div className="stack">
                     <div className="flexiblerow">
                         <h1 className="bold ultramassivetext bannertext1">Vaccinic is an all in one vaccine management tool</h1>
@@ -31,6 +31,34 @@ const Banner = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+            <SignUpPage/> */}
+            <div className="horizontalcenter bannertext10">
+                <div className="stack">
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div className="has-text-centered"><h1 className="ultramassivetext roboto">One place to manage all your vaccines</h1></div>
+                    <br/>
+                    <div className="has-text-centered"><h1 className="ultramassivetext">↓</h1></div>
+                    <br/>
+                    <br/>
+                </div>
+            </div>
+            <h1 className="heading2 horizontalcenter">With Vaccinic you can</h1>
+            <div className="flexiblerow spacearound ">
+            <div className="card2 grad1 stack pa3">
+                <img  src="https://img.icons8.com/plasticine/512/treatment-plan.png"/>
+                <h1>Keepp track of your past vaccines</h1>
+            </div>
+            <div className="card2 grad1 stack pa3">
+                <img  src="https://img.icons8.com/plasticine/512/new-moon.png"/>
+                <h1>Get insights and updates on future vaccines</h1>
+            </div>
+            <div className="card2 grad1 stack pa3">
+                <img src="https://img.icons8.com/plasticine/512/map-marker.png"/>
+                <h1>Get vaccine recommendations based on location</h1>
+            </div>
             </div>
             <SignUpPage/>
         </div>
