e4e721fbab46c980ab7e80d889314c54e815b277
abbazsr795 <abbazsr795@gmail.com>
2023-02-02 15:05:39 +0530

    yeet

--- src/Components/HomeComp.js
+++ src/Components/HomeComp.js
@@ -39,9 +39,9 @@ let HomeComp = ()=>{
         <div>
             <h1 className="horizontalcenter massivetext heading1">Welcome back {useremail.email}</h1>
             <div className="horizontalcenter flexiblerow spacearound">
-                <button onClick={()=>{toMyVaccines()}} ><div className="verticalcenter card1 grow grad3"><div className="horizontalcenter"><img className="img2" src="https://img.icons8.com/external-justicon-lineal-justicon/512/external-vaccine-science-justicon-lineal-justicon-1.png"/></div><h1 className="midtext">Your vaccines</h1></div></button>
-                <button onClick={()=>{toUserInfo()}} ><div className="verticalcenter card1 grow"><div className="horizontalcenter"><img className="img2" src="https://img.icons8.com/ios-glyphs/512/user.png"/></div><h1 className="midtext">User Info</h1></div></button>
-                <button onClick={()=>{toUserVaccines()}} ><div className="verticalcenter card1 grow "><div className="horizontalcenter"><img src="https://img.icons8.com/ios/512/list.png" className="img2"/></div><h1 className="midtext">Vaccine Index</h1></div></button>
+                <button className="bigbtn grow" onClick={()=>{toMyVaccines()}} ><div className="verticalcenter card1 grow grad3"><div className="horizontalcenter"><img className="img2" src="https://img.icons8.com/external-justicon-lineal-justicon/512/external-vaccine-science-justicon-lineal-justicon-1.png"/></div><h1 className="midtext">Your vaccines</h1></div></button>
+                <button className="bigbtn grow" onClick={()=>{toUserInfo()}} ><div className="verticalcenter card1 grow"><div className="horizontalcenter"><img className="img2" src="https://img.icons8.com/ios-glyphs/512/user.png"/></div><h1 className="midtext">User Info</h1></div></button>
+                <button className="bigbtn grow" onClick={()=>{toUserVaccines()}} ><div className="verticalcenter card1 grow "><div className="horizontalcenter"><img src="https://img.icons8.com/ios/512/list.png" className="img2"/></div><h1 className="midtext">Vaccine Index</h1></div></button>
             </div>
         </div>
 
