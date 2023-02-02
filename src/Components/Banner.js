58ebace4f4d54664def3749479badee848f059fb
abbazsr795 <abbazsr795@gmail.com>
2023-02-02 20:04:21 +0530

    header

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,18 +1,20 @@
 import React from "react";
 import SignIn from "./SignIn";
 import SignUpPage from "../Pages/SignUpPage";
+import Header from "./Header";
 
 const Banner = () => {
     return(
         <div>
-            <div className="horizontalcenter bannertext10">
-                <div className="stack">
+            <Header/>
+            <div className="horizontalcenter ">
+                <div className="stack pa0">
                     <div className="has-text-centered"><h1 className="bold ultramassivetext roboto">Welcome to <p className="lobster">VACCINIC</p>An all in tool to manage your vaccines</h1></div>
-                    <div className="has-text-centered"><h1 className="ultramassivetext bold">↓</h1></div>
+                    <div className="has-text-centered"><h1 className="ultramassivetext bold pa0">↓</h1></div>
                 </div>
             </div>
             <h1 className="heading2 horizontalcenter has-text-centered">With Vaccinic you can</h1>
-            <div className="flexiblerow spacearound ">
+            <div className="flexiblerow spacearound pa0">
             <div className="card2 grad1 stack pa3">
                 <img  src="https://img.icons8.com/plasticine/512/treatment-plan.png"/>
                 <h1>Keep track of your past vaccines</h1>
@@ -26,7 +28,11 @@ const Banner = () => {
                 <h1>Get vaccine recommendations based on location</h1>
             </div>
             </div>
-            <SignUpPage/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
         </div>
     )
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+
+    let navigate = useNavigate()
+
+    return(
+        <div className="horizontalcenter">
+            <div className="header row spacebetween flexiblerow">
+                <div className="row centeralign">
+                    <img className="logo" src="https://img.icons8.com/external-justicon-lineal-color-justicon/512/external-vaccine-science-justicon-lineal-color-justicon-1.png"/>
+                    <h1 className="ariel textsize44">VACCINIC</h1>
+                </div>
+                <div className="row centeralign gap10">
+                    <a onClick={() => navigate('/signin')}><h1 className="ariel textsize44 smalltext">Log In</h1></a>
+                    <a onClick={() => navigate('/signup')}><h1 className="ariel textsize44 smalltext whiteback btn99">Sign Up</h1></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
