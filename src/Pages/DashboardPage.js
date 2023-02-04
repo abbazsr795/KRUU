edf8882c208ed4dfc9604f08903245e465bbdf39
abbazsr795 <abbazsr795@gmail.com>
2023-02-04 10:01:21 +0530

    aded filter

--- src/Pages/DashboardPage.js
+++ src/Pages/DashboardPage.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 // import React from "react";
 import Footer from "../Components/Footer";
 import { UserLogData } from "../States/UserRelated";
+import { Switch } from "evergreen-ui"
 
 const DashboardPage = () => {
 
@@ -11,8 +12,35 @@ const DashboardPage = () => {
     let userd = useRecoilValue(UserLogData)
 
     return(
-        <div>
+        <div className="stack">
             <h1 className="massivetext heading1 horizontalcenter">Future vaccines</h1>
+            <div className="choosefutureregion  stack">
+                <h1>Planning to go somewhere?</h1>
+                <div className="flexiblerow">
+                    <p>Northern Asia</p>
+                    <Switch height={24} />
+                    <p>Central Asia</p>
+                    <Switch height={24} />
+                    <p>South America</p>
+                    <Switch height={24} />
+                    <p>Western Asia</p>
+                    <Switch height={24} />
+                    <p>Southern Asia</p>
+                    <Switch height={24} />
+                    <p>Eastern Asia</p>
+                    <Switch height={24} />
+                    <p>South Eastern Asia</p>
+                    <Switch height={24} />
+                    <p>Europe</p>
+                    <Switch height={24} />
+                    <p>Oceania</p>
+                    <Switch height={24} />
+                    <p>North America</p>
+                    <Switch height={24} />
+                    <p>Africa</p>
+                    <Switch height={24} />
+                </div>
+            </div>
         </div>
     )
 }
