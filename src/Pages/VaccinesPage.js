ef5111e3993fa92af048ac888af44b270670af45
abbazsr795 <abbazsr795@gmail.com>
2023-02-04 18:01:55 +0530

    huh

--- src/Pages/VaccinesPage.js
+++ src/Pages/VaccinesPage.js
@@ -24,12 +24,8 @@ const VaccinesPage = () => {
     return(
         <div>
             <h1 className="heading1 pa2 massivetext horizontalcenter">A list of all the vaccines in our database</h1>
-            <button onClick={()=>{getdata()}} >Get Vaccines</button>
+            <div className="horizontalcenter"><button className="button1 grow" onClick={()=>{getdata()}} >Get Vaccines</button></div>
             <div className="flexiblerow">
-                <div className="card2 verticalcenter grow">
-                    <h1>Haemophilus influenzae type b </h1>
-                    <h3>Hib can cause severe infections of both the lining of the brain and spinal cord (meningitis) and the bloodstream. Vaccines can prevent Hib disease H Influenza including Hib disease occurs mostly in children younger than 5 years old and adults 65 years or older. People with sickle cell disease, asplenia(no spleen) HIV infection Antibody and complement deficiency syndromes and cancer requiring treatment with chemo, radiation therapy or bone marrow stem call transplant are at a increased risk.</h3>
-                </div>
                 {
                     listofdata.map(p=>
                         <div className="card2 verticalcenter grow">
