3dd8dd4583a05225991f39c45242243a41ac0fae
amraaz <xylome45a@gmail.com>
2023-02-08 11:07:55 +0530

    added stuf

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -120,8 +120,6 @@ const SignUp = () => {
         <h1 className="horizontalcenter heading2">Sign Up</h1>
         <div className="horizontalcenter">
             <div className="">
-                <div className="horizontalcenter"><Link to='/signin' >Already have an account?</Link></div>
-                <br></br>
                 <div className="card4 lightblue pa3 verticalcenter">
                     <h1>Email</h1>
                     <input className="inputbox" onChange={emailSet} />
@@ -178,6 +176,7 @@ const SignUp = () => {
                    </div>
                    <br></br>
                    <br></br>
+                   <div className="horizontalcenter"><Link to='/signin' >Already have an account?</Link></div>
                 </div>
             </div>
         </div>
