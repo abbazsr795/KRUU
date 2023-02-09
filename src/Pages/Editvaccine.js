0d65a334090df337e2fd5c71bb7e92b18d0ac947
abbazsr795 <abbazsr795@gmail.com>
2023-02-09 16:49:55 +0530

    oops

--- src/Pages/Editvaccine.js
+++ src/Pages/Editvaccine.js
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom"
+import { Button } from "evergreen-ui"
 
 let Editvaccine =()=>{
     let params = useParams()
@@ -11,8 +12,21 @@ let Editvaccine =()=>{
     }
 
     return <>
-    <button onClick={()=>{Remove()}} >Remove Vaccine?</button>
-    <button onClick={()=>{navigate('/vaccines/adddose/'+name)}} >Add dose</button>
+    <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="horizontalcenter">
+            <div className="card21 stack pa3">
+                <Button intent="danger" appearance="primary" className="" onClick={()=>{Remove()}} >Remove Vaccine?</Button>
+                <br></br>
+                <Button intent="success" onClick={()=>{navigate('/vaccines/adddose/'+name)}} >Add dose</Button>
+            </div>
+        </div>
+    </div>
     </>
 
 }
