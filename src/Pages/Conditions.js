9f9ca5d8d78c24904264e1f5067cdc73650380c8
Shimar <shimar1167@gmail.com>
2023-02-10 17:51:24 +0530

    added stuff

--- src/Pages/Conditions.js
+++ src/Pages/Conditions.js
@@ -1,8 +1,52 @@
-// import data from "./Components/Country-Vaccine" 
+import data from "./MedicalConditions.json"
+import { collection, query, getDocs, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../FbStuff/fb";
+import { UserLogData } from "../States/UserRelated";
+import { useRecoilValue } from "recoil";
+
 let Conditions = ()=>{
 
+    let [listofdata, Setusestate] = useState()
+    let useremail = useRecoilValue(UserLogData)
+
+    let [config,setconfig] = useState(false)
+
+//     let getdata = async ()=>{
+//         const q = query(collection(db, "UserInfo"), where("email","==",useremail.email));
+
+//         const querySnapshot = await getDocs(q);
+//             querySnapshot.forEach((doc) => {
+//             let d = doc.data()
+//             Setusestate(d)
+//         });
+//         console.log(listofdata)
+//    }
+
+   useEffect(()=>{
+    // getdata()
+    // if (config!=="undefined"){
+    //     setconfig(false)
+    // }
+   },[])
+
     return <>
-    <h1 className="massivetext heading1 horizontalcenter">By Conditions</h1>
+        {
+            data.map(d=><div>
+                <p> {d.Vaccinename} </p>
+                <p> {d.preg} </p>
+                <p> {d.immunocompromised} </p>
+                <p> {d.hivcd4countl15p} </p>
+                <p> {d.hivcd4countg15p} </p>
+                <p> {d.asplenia} </p>
+                <p> {d.endstageRDorhd} </p>
+                <p> {d.Heartorlungdisease} </p>
+                <p> {d.Chronicliverdiease} </p>
+                <p> {d.diabetes} </p>
+                <p> {d.workhealthcare} </p>
+                <p> {d.gay} </p>
+            </div>)
+        }
     </>
 
 }
