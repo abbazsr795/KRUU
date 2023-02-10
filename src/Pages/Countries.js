e75bae91c3ebbcd12c8372482078cc3d8bdbeb00
amraaz <xylome45a@gmail.com>
2023-02-10 10:02:04 +0530

    added s

--- src/Pages/Countries.js
+++ src/Pages/Countries.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
+
+import countries from "./data.json"
+
 let Countries = ()=>{
 
     return <>
         <h1>Countries</h1>
+        {
+            countries.map(d=><div> {d.Country} </div>)
+        }
     </>
 
 }
