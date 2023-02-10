ca6a18d93c9b9b150db9a7a09fc7cdff53e0d5fd
amraaz <xylome45a@gmail.com>
2023-02-10 10:52:27 +0530

    added

--- src/Pages/Countries.js
+++ src/Pages/Countries.js
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
+import { Button, Pane, Popover } from "evergreen-ui";
 
 import countries from "./data.json"
 
 let Countries = ()=>{
 
+    let Constant_RoutineVaccine = [ "Chickenpox (Varicella)",  "Diphtheria-Tetanus-Pertussis",  "Flu (influenza)",  "Measles-Mumps-Rubella (MMR)",  "Polio",  "Shingles"]
+
     return <>
-<<<<<<< HEAD
-        <h1>Countries</h1>
-        {
-            countries.map(d=><div> {d.Country} </div>)
-        }
-=======
         <h1 className="massivetext heading1 horizontalcenter">By Countries</h1>
-        
->>>>>>> 19af29e170c515c3604a5027706d68e6e48ce699
+        {
+            countries.map(d=> <Popover content={<Pane width={300} height={400} >
+                <p> {d.Region} </p>
+                <p> {Constant_RoutineVaccine.map(f=><div>{f}</div>)} </p>
+            </Pane>} >
+                <Button>{d.Country}</Button>
+            </Popover>)
+        }
     </>
 
 }
--- src/Pages/countriesparams.js
+++ src/Pages/countriesparams.js
@@ -4,7 +4,8 @@ import { useParams } from "react-router-dom"
 let Countriesparams = ()=>{
 
     let params = useParams()
-    
+    let name = params.name
+    let desc = params.reg
 
     return <>
 
